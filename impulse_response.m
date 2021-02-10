b6eaf3bf78750c6add801ad74fbc02a335d72daa
Carolyn Du <cadu@hmc.edu>
2021-02-10 01:05:04 -0800

    Fix code and remove unnecessary file

--- impulse_response.m
+++ impulse_response.m
@@ -8,8 +8,10 @@ function [ h,t ] = impulse_response( x, y, fs )
 
 [X,f] = fdomain(x,fs);
 [Y,f] = fdomain(y,fs);
-H = X./Y;   % the calculated frequency response
+H = Y./X;   % the calculated frequency response
+% Carolyn Du 2-10-2021
 [h,t] = tdomain(H,fs);
+plot(t,h);
 
 end
 
